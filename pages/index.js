c2a95d8c73c8e852fd77a3534a92d2c6fa197a74
pababei <petrut.ababei@gmail.com>
2023-11-12 13:07:15 -0500

    Added API route and functions to retrieve and display data for posts of contact type

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,49 @@
 // Next.js will always look for index file
 import Link from "next/link";
 import Layout from "../components/layout";
-import { getSortedList } from "../lib/data";
+import { getSortedContactList, getSortedList } from "../lib/data";
 
 // define getStaticProps()
 export async function getStaticProps() {
   const allData = await getSortedList();
+  const allContactData = await getSortedContactList();
   return {
-    props: { allData },
+    props: { allData, allContactData },
   };
 }
 
-export default function Home({ allData }) {
+export default function Home({ allData, allContactData }) {
   return (
     <Layout>
       <h1>Wordpress Headless App Demo</h1>
-      <div className="list-group">
-        {allData.map(({ id, title }) => (
-          <Link
-            key={id}
-            href={`/posts/${id}`}
-            className="list-group-item list-group-item-action"
-          >
-            {title}
-          </Link>
-        ))}
-      </div>
+      <section>
+        <h2>Posts</h2>
+        <div className="list-group">
+          {allData.map(({ id, title }) => (
+            <Link
+              key={id}
+              href={`/posts/${id}`}
+              className="list-group-item list-group-item-action"
+            >
+              {title}
+            </Link>
+          ))}
+        </div>
+      </section>
+      <section>
+        <h2>Contacts</h2>
+        <div className="list-group">
+          {allContactData.map(({ id, title }) => (
+            <Link
+              key={id}
+              href={`/contacts/${id}`}
+              className="list-group-item list-group-item-action"
+            >
+              {title}
+            </Link>
+          ))}
+        </div>
+      </section>
     </Layout>
   );
 }
